7f414b40bfe86e36f2c44ed150347607307ad9f6
Alex <31666303+khabaznak@users.noreply.github.com>
2024-01-20 20:57:37 +0000

    feat: general structure of feature navigation

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,5 +14,13 @@ router.get('/register',(req, res)=> {
     res.render('register',{layout:'main', title:'KimioLink User Registration'});
 });
 
+router.post('/registerNewUser', (req,res) =>{
+    res.render('register_success',{layout:'main', title:'Successful registration'});
+});
+
+router.get('/home',(req,res) =>{
+    res.render('homepage',{layout:'home',title:'KimioLink Patient Home', patientId:'12345'});
+});
+
 // Export the router
 module.exports = router;
