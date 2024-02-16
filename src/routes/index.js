bcd51f5ee4ea4546bdea905772559b1c76ecad8d
Alex <31666303+khabaznak@users.noreply.github.com>
2024-02-16 03:27:32 +0000

    feature: added files for the plan and profile.

--- src/routes/index.js
+++ src/routes/index.js
@@ -24,8 +24,33 @@ router.post('/registerNewUser', (req,res) =>{
 });
 
 router.get('/home',(req,res) =>{
+    console.log('Home');
     res.render('homepage',{layout:'home',title:'KimioLink Patient Home', patientId:'12345'});
 });
+
+router.get('/patient_profile', (req, res) => {
+    res.render('patient_profile_admin', 
+    {layout:'home', title:'KimioLink Patient Profile', patientId:'12345'});
+});
+
+router.post('/patient_profile', (req, res) => {
+    console.log('received this patient profile: '. req.body);
+    res.render('patient_profile_admin',
+    {layout:'home', title:'KimioLink Patient profile', patient_id:'12345', toast:'Attempted to save profile'});
+});
+
+router.get('/show_add_stage_form', (req, res) => {
+    console.log('Showing the add new stage capture form');
+    res.render('partials/add_new_stage_form',
+    {layout:false, title:'KimioLink Oncological Plan', patient_id:'123345'});
+});
+
+router.get('/oncological_plan', (req, res) => {
+    console.log('Oncological planner');
+        res.render('oncological_plan',
+        {layout:'home', title:'KimioLink Oncological Plan', patient_id:'12345'});
+});
+
 
 // Export the router
 module.exports = router;
