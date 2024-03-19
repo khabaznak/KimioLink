53d99fdf9838fc317ddfe7c2dc51797c75f83032
Alex <31666303+khabaznak@users.noreply.github.com>
2024-03-19 05:08:33 +0000

    feature: Enabled Google Material Design 3

---
+++ src/assets/styles/material.js
@@ -0,0 +1,9 @@
+// material.js
+// Here you will import all material design 3 components you wish to use in the application.
+// Then you will "rollup" this and call it from the HTML 
+import '@material/web/button/filled-button.js';
+import '@material/web/button/outlined-button.js';
+import '@material/web/button/text-button.js';
+import '@material/web/checkbox/checkbox.js';
+import '@material/web/textfield/filled-text-field.js';
+import '@material/web/textfield/outlined-text-field.js';
